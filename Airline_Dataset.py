41327893b02448006d5f4b3e4731ac9513e2eca2
SohilaAbdallah <110264357+SohilaAbdallah@users.noreply.github.com>
2023-05-08 01:25:13 +0300

    Add files via upload

---
+++ Airline_Dataset.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as mn
+
+DataFile =  pd.read_csv("C:\College\Semster 6\Intro to Artificial Intelligencetest.csv")
